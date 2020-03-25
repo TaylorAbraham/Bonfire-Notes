873e367049c3357debcb3995a71b1692384bc337
Taylor Abraham <taylor.abra8@gmail.com>
2020-03-25 13:53:04 -0400

    Fix styles and add signup handler boilerplate

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const SignUp = () => {
+import * as ROUTES from '../../constants/routes';
+
+const SignUpPage = () => {
+  const onSubmit = (event) => {
+
+  };
+
+  const onChange = (event) => {
+
+  };
+
   return (
     <div>
-      SignUp
+      <h1>Sign Up</h1>
+      <form onSubmit={onSubmit}>
+
+      </form>
     </div>
   );
 };
 
-export default SignUp;
+export const SignUpLink = () => (
+  <p>
+    Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up!</Link>
+  </p>
+);
+
+export default SignUpPage;
