f5393817f8d6b2e23c7e6c9c9bcf0cc0ed7ebde5
Taylor Abraham <taylor.abra8@gmail.com>
2020-03-25 13:41:01 -0400

    Add material ui

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+
+import './styles.scss';
 import * as ROUTES from '../../constants/routes';
 
-const Navigation = () => (
-  <div>
-    <ul>
-      <li>
-        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.LANDING}>Landing</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.HOME}>Home</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ACCOUNT}>Account</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ADMIN}>Admin</Link>
-      </li>
-    </ul>
-  </div>
-);
+const Navigation = () => {
+  return (
+    <AppBar className="cn-nav" position="static">
+      <Toolbar>
+        <Button color="inherit">
+         <Link className="cn-nav__link" to={ROUTES.SIGN_IN}>Sign In</Link>
+        </Button>
+        <Button color="inherit">
+         <Link className="cn-nav__link" to={ROUTES.LANDING}>Landing</Link>
+        </Button>
+        <Button color="inherit">
+          <Link className="cn-nav__link" to={ROUTES.HOME}>Home</Link>
+        </Button>
+        <Button color="inherit">
+         <Link className="cn-nav__link" to={ROUTES.ACCOUNT}>Account</Link>
+        </Button>
+        <Button color="inherit">
+          <Link className="cn-nav__link" to={ROUTES.ADMIN}>Admin</Link>
+        </Button>
+      </Toolbar>
+    </AppBar>
+  )
+};
 
 export default Navigation;
